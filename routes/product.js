7c16da8c97c4e650a828479a5eae363a780b8eec
christopher deo <chris@christopherdeo.com>
2023-01-05 07:51:27 -0600

    found and fixed bug in HTTP PUT request

--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,7 @@ const AWS = require('aws-sdk');
 AWS.config.update({
     region: 'us-east-1'
 });
+
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const dynamodbTableName = 'product-inventory';
 
